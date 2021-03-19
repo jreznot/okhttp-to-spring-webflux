bdd76c5e669959c2a9e1ac7dcc14d16c3f157f11
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2021-03-19 20:53:42 +0300

    Fix URL

--- src/test/java/com/example/demo/OkhttpToServerTest.java
+++ src/test/java/com/example/demo/OkhttpToServerTest.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
-import okhttp3.*;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,7 +17,7 @@ class OkhttpToServerTest {
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
-                .url(HttpUrl.get("http://localhost/functional/mono"))
+                .url(new URL("http://localhost/functional/mono"))
                 .build();
 
         Call call = client.newCall(request);
